e858d5b3cc1d145cd762f59f517ca89eddb299c6
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-12-13 16:33:31 +0700

    feat: enable model loading and prediction endpoint in FastAPI

--- python/app/main.py
+++ python/app/main.py
@@ -4,7 +4,7 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
-# import tensorflow as tf
+import tensorflow as tf
 from .utils import predict_image, class_names
 from dotenv import load_dotenv
 
@@ -26,20 +26,20 @@ async def root():
 MODEL_PATH = os.getenv('MODEL_PATH', 'default_model_path')
 
 # Load the model once at startup
-# model = tf.keras.models.load_model(MODEL_PATH)
+model = tf.keras.models.load_model(MODEL_PATH)
 
-# @app.post("/predict/")
-# async def predict(file: UploadFile = File(...)):
-#     try:
-#         contents = await file.read()
-#         result = predict_image(contents, model, class_names)
-#         return {"result": result}
-#     except ValueError as ve:
-#         # No hand detected
-#         raise HTTPException(status_code=400, detail=str(ve))
-#     except Exception as e:
-#         # General exception
-#         raise HTTPException(status_code=500, detail="Internal Server Error")
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        result = predict_image(contents, model, class_names)
+        return {"result": result}
+    except ValueError as ve:
+        # No hand detected
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        # General exception
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
